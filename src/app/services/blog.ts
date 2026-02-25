f71cf3d3fecc4fb352e35c5a1b3fc978ee919ee1
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-02-25 16:23:39 +0100

    Añadimos el array inicial y los metodos getAll e insert en el servicio

--- src/app/services/blog.ts
+++ src/app/services/blog.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { INoticia } from '../interfaces/inoticia';
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root', // Esto hace que el servicio esté disponible en toda la app
 })
-export class Blog {
-  
+export class BlogService {
+  private misNoticias: INoticia[] = []
+
+  getAll() :INoticia[] {
+    return this.misNoticias
+  }
+
+  insert(noticia:INoticia): string{
+    this.misNoticias.push(noticia)
+    return "Noticia insertada correctamente"
+  }
 }
